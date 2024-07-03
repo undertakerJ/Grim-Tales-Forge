767743ba73b11a351f48876c2fd99db48699d6eb
undertakerJ <justghost1242@gmail.com>
2024-07-04 00:28:00 +0300

    Added Armor Shred effect and enchant, some code cleanup(thanks to Daripher).

--- src/main/java/net/undertaker/grimtales/item/ModCreativeTabs.java
+++ src/main/java/net/undertaker/grimtales/item/ModCreativeTabs.java
@@ -11,30 +11,30 @@ import net.undertaker.grimtales.GrimTales;
 import net.undertaker.grimtales.block.ModBlocks;
 
 public class ModCreativeTabs {
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
-            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, GrimTales.MOD_ID);
+  public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+      DeferredRegister.create(Registries.CREATIVE_MODE_TAB, GrimTales.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> GRIMTALES_TAB =
-            CREATIVE_MODE_TABS.register("grimtales_tab",
-                    () -> CreativeModeTab.builder()
-                            .icon(() -> new ItemStack(ModItems.RAW_CEBBITE_ORE.get()))
-                            .title(Component.translatable("creativetab.grimtales_tab"))
-                            .displayItems((itemDisplayParameters, output) -> {
-                                //items
-                                output.accept(ModItems.RAW_CEBBITE_ORE.get());
-                                output.accept(ModItems.CEBBITE_INGOT.get());
-                                output.accept(ModItems.CEBBITE_PICKAXE.get());
-                                output.accept(ModItems.CEBBITE_SWORD.get());
-                                //blocks
-                                output.accept(ModBlocks.CEBBITE_ORE.get());
-                                output.accept(ModBlocks.DEEPSLATE_CEBBITE_ORE.get());
-                                output.accept(ModBlocks.SCULK_CEBBITE_ORE.get());
-                                output.accept(ModBlocks.CEBBITE_BLOCK.get());
-                            })
-                            .build());
+  public static final RegistryObject<CreativeModeTab> GRIMTALES_TAB =
+      CREATIVE_MODE_TABS.register(
+          "grimtales_tab",
+          () ->
+              CreativeModeTab.builder()
+                  .icon(() -> new ItemStack(ModItems.RAW_CEBBITE_ORE.get()))
+                  .title(Component.translatable("creativetab.grimtales_tab"))
+                  .displayItems(
+                      (itemDisplayParameters, output) -> {
+                        // items
+                        ModItems.ITEMS.getEntries().stream()
+                            .map(RegistryObject::get)
+                            .forEach(output::accept);
+                        // blocks
+                        ModBlocks.BLOCKS.getEntries().stream()
+                            .map(RegistryObject::get)
+                            .forEach(output::accept);
+                      })
+                  .build());
 
-
-    public static void register(IEventBus eventBus){
-        CREATIVE_MODE_TABS.register(eventBus);
-    }
+  public static void register(IEventBus eventBus) {
+    CREATIVE_MODE_TABS.register(eventBus);
+  }
 }
